6bdc9ce52b922732cc6e8fcfbc3aa178c829465c
Lora <plutovka-z@rambler.ru>
2021-09-08 20:22:51 +0300

    add RoomDto

--- src/main/java/ru/example/letterflow/domain/dto/RoomDto.java
+++ src/main/java/ru/example/letterflow/domain/dto/RoomDto.java
@@ -1,4 +1,8 @@
 package ru.example.letterflow.domain.dto;
 
 public class RoomDto {
+    private Long roomId;
+    private Long userId;
+    private Boolean personal;
+    private String roomName;
 }
