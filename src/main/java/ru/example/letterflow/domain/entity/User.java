be8abae6053844560054da17c6a7a552a3ba4784
Lora <plutovka-z@rambler.ru>
2021-09-09 20:59:59 +0300

    fix2

--- src/main/java/ru/example/letterflow/domain/entity/User.java
+++ src/main/java/ru/example/letterflow/domain/entity/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "userId")
     private Long userId;
 
