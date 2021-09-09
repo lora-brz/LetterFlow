bc8ae4407e8c311349acae5e194781853e4bf0dd
Lora <plutovka-z@rambler.ru>
2021-09-09 19:44:07 +0300

    fix

--- src/main/java/ru/example/letterflow/domain/dto/UserDto.java
+++ src/main/java/ru/example/letterflow/domain/dto/UserDto.java
@@ -5,6 +5,7 @@ import ru.example.letterflow.domain.entity.Room;
 import java.util.List;
 
 public class UserDto {
+    private Long userId;
     private String login;
     private Character character;
     private List<Room> rooms;
