4bc7aa87e0770d385b329b041d56ddd607693f21
Lora <plutovka-z@rambler.ru>
2021-10-11 23:56:53 +0300

    fix BotController

--- src/main/java/ru/example/letterflow/controller/BotController.java
+++ src/main/java/ru/example/letterflow/controller/BotController.java
@@ -1,116 +1,133 @@
 package ru.example.letterflow.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import ru.example.letterflow.domain.dto.RoomDto;
-import ru.example.letterflow.domain.dto.UserDto;
-import ru.example.letterflow.domain.entity.Enum.Role;
-import ru.example.letterflow.domain.entity.Room;
 import ru.example.letterflow.domain.entity.User;
-import ru.example.letterflow.exceptions.*;
-import ru.example.letterflow.repository.RoomRepo;
-import ru.example.letterflow.service.RoomService;
-import ru.example.letterflow.service.UserService;
+import ru.example.letterflow.exceptions.ImpossibleActionException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+//import ru.example.letterflow.service.BotService;
 
 @RestController
 @RequestMapping("/bot")
 public class BotController {
-
-    private final RoomService roomService;
-    private final RoomRepo roomRepo;
-    private final UserService userService;
-
-    @Autowired
-    public BotController(RoomService roomService, RoomRepo roomRepo, UserService userService) {
-        this.roomService = roomService;
-        this.roomRepo = roomRepo;
-        this.userService = userService;
+    public static void main(String[] args) throws ImpossibleActionException {
+        System.out.println(new BotController().parseCommand("help"));
     }
 
-    @GetMapping("/room_create_${roomName}")
-    @PreAuthorize("hasAuthority('user')")
-    public RoomDto createRoom(@RequestBody User user,
-                              @PathVariable String roomName) throws InsufficientAccessRightsException, RoomAlreadyExistException {
-        return roomService.createRoom(user, roomName, false);
+//    private final BotService botService;
+//    private final String command;
+//
+//    @Autowired
+//    public BotController(BotService botService, String command) {
+//        this.botService = botService;
+//        this.command = command;
+//    }
+
+    @GetMapping
+    public void possibleActions(@RequestBody User user,
+                                @RequestBody String command) throws ImpossibleActionException {
+        Map<String, String> mapCommand = parseCommand(command);
+        String service = mapCommand.get("service");
+        String action = mapCommand.get("action");
+        String roomName = mapCommand.get("roomName");
+        String login = mapCommand.get("login");
+        Boolean person = Boolean.valueOf(mapCommand.get("person"));
+        Long milliseconds = Long.valueOf(mapCommand.get("milliseconds"));
+        String role = mapCommand.get("role");
+        String nameChannel = mapCommand.get("nameChannel");
+        Boolean views = Boolean.valueOf(mapCommand.get("views"));
+        Boolean likes = Boolean.valueOf(mapCommand.get("likes"));
+
+        switch (service){
+            case "room":
+                roomCommand(action, roomName, person, login, milliseconds);
+                break;
+            case "user":
+                userCommand(action, login, milliseconds, role);
+                break;
+            case "yBot":
+                botCommand(action, nameChannel, views, likes);
+                break;
+        }
     }
 
-    @GetMapping("/room_create_${roomName}-p")
-    @PreAuthorize("hasAuthority('user')")
-    public RoomDto createPersonalRoom(@RequestBody User user,
-                              @PathVariable String roomName) throws InsufficientAccessRightsException, RoomAlreadyExistException {
-        return roomService.createRoom(user, roomName, true);
+    @PutMapping
+    public Map<String, String> parseCommand(String command) throws ImpossibleActionException {
+        ArrayList<String> listCommand = new ArrayList<>();
+        Collections.addAll(listCommand, command.split(" "));
+        Map<String, String> mapCommand = new HashMap();
+        if(listCommand.size()==1 && listCommand.get(0).equals("help")){
+            mapCommand.put("service", "yBot");
+            mapCommand.put("action","help");
+            return mapCommand;
+        }
+        try{
+            mapCommand.put("service", listCommand.get(0));
+            mapCommand.put("action", listCommand.get(1));
+
+            if (mapCommand.get("service").equals("room") && listCommand.size() > 2) {
+                mapCommand.put("roomName", listCommand.get(2));
+                for (String com : listCommand) {
+                    if (com.equals("-l")) {
+                        mapCommand.put("login", listCommand.get(listCommand.indexOf(com) + 1));
+                    }
+                    if (com.equals("-c")) {
+                        mapCommand.put("person", "person");
+                    }
+                    if (com.equals("-m")) {
+                        mapCommand.put("milliseconds", listCommand.get(listCommand.indexOf(com) + 1));
+                    }
+                }
+            }
+            if(mapCommand.get("service").equals("user")){
+                mapCommand.put("login", listCommand.get(2));
+                for (String com : listCommand) {
+                    if (com.equals("-n")) {
+                        mapCommand.put("role", "moderator");
+                    }
+                    if (com.equals("-d")) {
+                        mapCommand.put("role", "user");
+                    }
+                    if (com.equals("-m")) {
+                        mapCommand.put("milliseconds", listCommand.get(listCommand.indexOf(com) + 1));
+                    }
+                }
+            }
+
+            if(mapCommand.get("service").equals("yBot") && mapCommand.get("action").equals("find")){
+                for (String com : listCommand) {
+                    if (com.equals("-k")) {
+                        mapCommand.put("nameChannel", listCommand.get(listCommand.indexOf(com) + 1));
+                    }
+                    if (com.equals("-v")) {
+                        mapCommand.put("views", "views");
+                    }
+                    if (com.equals("-l")) {
+                        mapCommand.put("likes", "likes");
+                    }
+                }
+            }
+
+        } catch (Exception e){
+            throw new ImpossibleActionException("Ввели неверную команду");
+        }
+        return mapCommand;
     }
 
-    @DeleteMapping("/room_remove_${roomName}")
-    @PreAuthorize("hasAuthority('user')")
-    public String removeRoom(@RequestBody User user,
-                             @RequestParam String roomName) throws InsufficientAccessRightsException {
-        return roomService.deleteRoom(user, roomRepo.findByRoomName(roomName));
+    public void roomCommand(String action, String roomName, Boolean person, String login, Long milliseconds){
+
     }
 
-    @PutMapping("/room_rename_${roomName}")
-    @PreAuthorize("hasAuthority('user')")
-    public RoomDto renameRoom(@RequestBody User user,
-                              @RequestBody Room room,
-                              @PathVariable String roomName) throws InsufficientAccessRightsException, RoomAlreadyExistException {
-        return roomService.renameRoom(user, room, roomName);
+    public void userCommand(String action, String login, Long milliseconds, String role){
+
     }
 
-    @PutMapping("/room_connect_${roomName}")
-    @PreAuthorize("hasAuthority('user')")
-    public UserDto connectRoom(@RequestParam Long userId,
-                              @PathVariable String roomName,
-                              @RequestParam String login) throws InsufficientAccessRightsException, UserNotFoundException {
-        return userService.addUserInRoom(userId, roomName, login);
-    }
-    @PutMapping("/room_connect_${roomName}-l${login}")
-    @PreAuthorize("hasAuthority('user')")
-    public UserDto inRoom(@RequestParam Long userId,
-                              @PathVariable String roomName,
-                              @PathVariable String login) throws InsufficientAccessRightsException, UserNotFoundException {
-        return userService.addUserInRoom(userId, roomName, login);
+    public void botCommand(String action, String nameChannel, Boolean views, Boolean likes){
+
     }
 
-    @PutMapping("/room_disconnect_${roomName}")
-    @PreAuthorize("hasAuthority('every')")
-    public UserDto disconnectRoom(@RequestParam Long userId,
-                             @PathVariable String roomName) throws InsufficientAccessRightsException, UserNotFoundException, ImpossibleActionException {
-        return userService.deleteUserInRoom(userId, roomName, userService.findUserById(userId).getLogin());
-    }
-
-    @PutMapping("/room_disconnect_${roomName}-l${login}")
-    @PreAuthorize("hasAuthority('every')")
-    public UserDto outRoom(@RequestParam Long userId,
-                             @PathVariable String roomName,
-                             @PathVariable String login) throws InsufficientAccessRightsException, UserNotFoundException, ImpossibleActionException {
-        return userService.deleteUserInRoom(userId, roomName, login);
-    }
-
-    @PutMapping ("/user_rename_${newLogin}")
-    @PreAuthorize("hasAuthority('every')")
-    public UserDto renameUser(@RequestParam Long userId,
-                              @PathVariable String newLogin) throws UserAlreadyExistException, UserNotFoundException, InsufficientAccessRightsException {
-        return userService.editName(userId, userService.findUserById(userId).getLogin(), newLogin);
-    }
-
-    @PutMapping ("/user_rename_${login}_${newLogin}")
-    @PreAuthorize("hasAuthority('every')")
-    public UserDto renameUser(@RequestParam Long userId,
-                              @PathVariable String login,
-                              @PathVariable String newLogin) throws UserAlreadyExistException, UserNotFoundException, InsufficientAccessRightsException {
-        return userService.editName(userId, login, newLogin);
-    }
-
-    @PutMapping("/user_moderator_${login}-n")
-    @PreAuthorize("hasAuthority('moderator')")
-    public UserDto makeModerator(@PathVariable String login) throws InsufficientAccessRightsException, UserNotFoundException {
-        return userService.editRole(userService.findUserByLogin(login).getUserId(), login, Role.MODERATOR.name());
-    }
-
-    @PutMapping("/user_moderator_${login}-d")
-    @PreAuthorize("hasAuthority('moderator')")
-    public UserDto makeUser(@PathVariable String login) throws InsufficientAccessRightsException, UserNotFoundException {
-        return userService.editRole(userService.findUserByLogin(login).getUserId(), login, Role.USER.name());
-    }
 }
